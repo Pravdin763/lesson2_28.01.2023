108a21bcde088590437d395be93a557347185dbf
igor <igor.pravdin163@gmail.com>
2023-01-28 19:09:57 +0400

    1

---
+++ lesson2.java
@@ -0,0 +1,18 @@
+public class lesson2 {
+    public static void main(String[] args) 
+    {
+        if(args.length == 0) System.out.println("no data");
+        else System.out.println(Palindrome(args[0]));
+    }
+ 
+    private static boolean Palindrome(String data) 
+    {
+        data = data.toLowerCase();
+        data = data.replace(" ", "");
+        int dataLen = data.length();
+        for(int i = 0; i < dataLen / 2; i++)
+            if(data.charAt(i) != data.charAt(dataLen - i - 1))
+                return false;
+        return true;
+    }
+}
